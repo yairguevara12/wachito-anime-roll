13b40852cc65bde89ff7bce125574a514ae1cb1e
yairguevara12 <guevaradelgadoyair12@gmail.com>
2023-02-19 12:10:38 -0500

    fixing the item menu

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,36 @@
 import React from "react";
-import "../style/home.css" ;
-import "../style/util.css" ;
+import "../style/home.css";
+import "../style/util.css";
 
 import Header from "../components/header.js";
 import Menu from "../components/menu.js";
 import HomeBody from "../components/homeBody.js";
-export default function Home(){
+export default function Home() {
 
     const [isShownMenu, setIsShownMenu] = React.useState(false);
-
-    function handleSetIsShownMenu(){
+    const lockWindow = React.useRef(null);
+    function handleSetIsShownMenu() {
         setIsShownMenu(prev => !prev);
+
     }
 
-    return( 
-        <div className="home">
-           
-        <Header isShownMenu={isShownMenu} handleSetIsShownMenu={handleSetIsShownMenu} />
+    React.useEffect(() => {
+        console.log(isShownMenu);
 
-        {isShownMenu && <Menu /> }
+        isShownMenu ? lockWindow.current.parentElement.parentElement.style.overflow = 'hidden'
+            : lockWindow.current.parentElement.parentElement.style.overflow = 'auto';
 
-        <HomeBody></HomeBody>
+
+
+    }, [isShownMenu])
+    return (
+        <div ref={lockWindow} className="home">
+
+            <Header isShownMenu={isShownMenu} handleSetIsShownMenu={handleSetIsShownMenu} />
+
+            {isShownMenu && <Menu />}
+
+            <HomeBody></HomeBody>
 
         </div>
     );
