94e280638777555dd9978b0812d221ad27341443
yairguevara12 <guevaradelgadoyair12@gmail.com>
2023-02-21 15:17:43 -0500

    creating a component

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "../style/search.css";
 import "../style/util.css";
-export default function Search(){
-    return(<div className="search-container flex flex-justify-center">
+import ResultCard from "../components/resultCard";
+export default function Search() {
+    return (
+    <div className="search-container flex flex-justify-center">
         <input className="" type="search"></input>
+        <div className="search-result">
+
+        </div>
+
     </div>)
 }
