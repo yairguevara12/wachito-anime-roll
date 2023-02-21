4a9acdac93b621daf77ced14774fb1b5bbb88917
yairguevara12 <guevaradelgadoyair12@gmail.com>
2023-02-21 11:59:48 -0500

    changing the body

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import Home from './pages/home';
 import './style/style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import "./style/home.css";
+import "./style/util.css";
 
+import Header from "./components/header";
+import Menu from "./components/menu.js";
 
 function App() {
+  const [isShownMenu, setIsShownMenu] = React.useState(false);
+  const lockWindow = React.useRef(null);
+  function handleSetIsShownMenu() {
+    setIsShownMenu(prev => !prev);
+
+  }
+
+  React.useEffect(() => {
+
+    isShownMenu ? lockWindow.current.parentElement.parentElement.style.overflow = 'hidden'
+      : lockWindow.current.parentElement.parentElement.style.overflow = 'auto';
+
+
+
+  }, [isShownMenu])
   return (
-    <div className='app' >
+    <div ref={lockWindow} className='app' >
+      <Header isShownMenu={isShownMenu} handleSetIsShownMenu={handleSetIsShownMenu} />
+
+      {isShownMenu && <Menu />}
       <Home />
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,9 @@
 import React from "react";
-import "../style/home.css";
-import "../style/util.css";
 
-import Header from "../components/header.js";
-import Menu from "../components/menu.js";
 import HomeBody from "../components/homeBody.js";
 export default function Home() {
 
-    const [isShownMenu, setIsShownMenu] = React.useState(false);
+     /* const [isShownMenu, setIsShownMenu] = React.useState(false);
     const lockWindow = React.useRef(null);
     function handleSetIsShownMenu() {
         setIsShownMenu(prev => !prev);
@@ -21,16 +17,16 @@ export default function Home() {
 
 
 
-    }, [isShownMenu])
+    }, [isShownMenu]) */
     return (
-        <div ref={lockWindow} className="home">
+        <div  className="home">
 
-            <Header isShownMenu={isShownMenu} handleSetIsShownMenu={handleSetIsShownMenu} />
+           {/*  <Header isShownMenu={isShownMenu} handleSetIsShownMenu={handleSetIsShownMenu} />
 
-            {isShownMenu && <Menu />}
+            {isShownMenu && <Menu />} */}
 
             <HomeBody></HomeBody>
 
         </div>
-    );
+    ); 
 }
